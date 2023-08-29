8e16ba053cdd5e4aef65da712538e9c4aa4d10b5
huahua <huazixu1998@gmail.com>
2023-08-29 22:55:42 +0800

    add repos

--- modules/relative_pos_embeddings.py
+++ modules/relative_pos_embeddings.py
@@ -1,8 +1,8 @@
 import torch
 import torch.nn as nn
 class RotaryPositionalEmbedding(nn.Module):
-    def __init__(self, *args, **kwargs) -> None:
-        super().__init__(*args, **kwargs)
+    def __init__(self) -> None:
+        super().__init__()
     def forward(self):
         x=0
 
