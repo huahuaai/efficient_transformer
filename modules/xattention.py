6a87fc28e4f14cf7d154d3fada317c0ea534b72d
hhai <924024667@qq.com>
2023-08-30 16:50:05 +0800

    add pos coding

--- modules/xattention.py
+++ modules/xattention.py
@@ -1,7 +1,13 @@
 import torch
 import torch.nn as nn
 class GAU(nn.Module):
-    def __init__(self, *args, **kwargs) -> None:
-        super().__init__(*args, **kwargs)
+    def __init__(self, ):
+        super().__init__()
     def forward(self):
         x=0
+
+class maGAU(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    
